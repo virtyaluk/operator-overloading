85d969de72de3844ea228efe1e13813b247f319d
Bohdan Shtepan <winsatana@ya.ru>
2015-12-28 17:42:28 +0200

    Add Python example

---
+++ python/example.py
@@ -0,0 +1,18 @@
+class Vector:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    # Overrides + (a + b).
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y);
+
+    # Overrides - (a - b).
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y);
+
+v1 = Vector(2, 3)
+v2 = Vector(4, 6)
+
+assert (v1 + v2).x == 6 and (v1 + v2).y == 9, "Condition not true."
+assert (v2 - v1).x == 2 and (v2 - v1).y == 3, "Condition not true."
