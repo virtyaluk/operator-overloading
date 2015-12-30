eb35bafbf7870da2d533e1c15461451ea75adc6e
Bohdan Shtepan <winsatana@ya.ru>
2015-12-30 15:15:42 +0200

    Code cleanup

--- perl/example.pl
+++ perl/example.pl
@@ -2,33 +2,35 @@
 use Carp::Assert;
 
 {
-	package vector;
-	use overload
-		'+' => "plus",
-		'-' => "minus";
-	sub new {
-		my ($class, $x, $y) = @_;
-		my %info;
-		$info{x} = $x;
-		$info{y} = $y;
-		bless \%info, $class;
-	}
-	sub area {
-		my ($self) = @_;
-		$$self{x} * $$self{y}
-	}
-	
-	# Overrides + (a + b).
-	sub plus {
-		my ($this, $that) = @_;
-		new vector($$this{x} + $$that{x}, $$this{y} + $$that{y});
-	}
-	
-	# Overrides - (a - b).
-	sub minus {
-		my ($this, $that) = @_;
-		new vector($$this{x} - $$that{x}, $$this{y} - $$that{y});
-	}
+  package vector;
+  use overload
+    '+' => "plus",
+    '-' => "minus";
+
+  sub new {
+    my ($class, $x, $y) = @_;
+    my %info;
+    $info{x} = $x;
+    $info{y} = $y;
+    bless \%info, $class;
+  }
+
+  sub area {
+    my ($self) = @_;
+    $$self{x} * $$self{y}
+  }
+
+  # Overrides + (a + b).
+  sub plus {
+    my ($this, $that) = @_;
+    new vector($$this{x} + $$that{x}, $$this{y} + $$that{y});
+  }
+
+  # Overrides - (a - b).
+  sub minus {
+    my ($this, $that) = @_;
+    new vector($$this{x} - $$that{x}, $$this{y} - $$that{y});
+  }
 }
 
 $v1 = new vector(3,6);
